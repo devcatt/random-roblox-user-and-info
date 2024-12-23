599ce31f38701ad29d5daf087f446ba322134382
cat <104304061+devcatt@users.noreply.github.com>
2024-12-23 08:34:44 +0100

    Add files via upload

---
+++ randomrobloxuser/betterGenerating.ts
@@ -0,0 +1,40 @@
+let fetchlink: string = "https://users.roblox.com/v1/users/"
+let userhasfriends: boolean = false;
+let randomID: number = Math.floor(Math.random() * 7737784538)
+
+function getRandomUser() { 
+    fetchlink += String(randomID)
+    console.log(fetchlink)
+}
+
+function fetchUser() {
+    getRandomUser()
+    
+    fetch(fetchlink).then(response => response.json()).then(async data => {
+        if (data.errors) {
+            console.error("something went wrong")
+        }
+        console.log(`display name: ${data.displayName}`)
+        console.log(`username: ${data.name}`)
+        console.log(`banned: ${data.isBanned}`)
+    })
+}
+
+
+
+function getFriends() {
+    fetchUser()
+    
+    try {
+        fetch(`https://friends.roblox.com/v1/users/${randomID}/friends/count`).then(response => response.json()).then(data => {
+            if (data.count > 0) {
+                console.log(`amount of friends: ${data.count}`)
+            } else if (data.count == 0) {
+                console.log("user doesn't have friends")
+            }
+        })
+    }
+    catch (error) {console.error(error)}
+}
+
+getFriends()
